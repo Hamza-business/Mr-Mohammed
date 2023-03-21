0ab01881288699bc613f037930d64d475e47acdb
Hamza Elkotb <hamzadev1business@gmail.com>
2023-03-22 00:39:42 +0300

    navbaar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,22 @@
 function showLects(){
     document.querySelector('.menu').classList.toggle('noactive')
 }
+
+function navshow(){
+    if (document.getElementById('navbarBasicExample').style.display == 'block'){
+        document.getElementById('navbarBasicExample').style.display = 'none';
+    }
+
+    else{
+        document.getElementById('navbarBasicExample').style.display = 'block';
+    }
+}
+
+
+let nav = document.querySelector('.navbar');
+if(nav){
+    window.addEventListener('scroll',()=>{
+        if(window.scrollY == 0) nav.classList.remove('moved')
+        else nav.classList.add('moved')
+    })
+}
